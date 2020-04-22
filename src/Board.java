609a114130786c4db4f2e25d50f70a25d0b380a4
bhargav <bhargavreddypingle@gmail.com>
2020-04-22 12:00:00 -0500

    Added Board class to manage the game board

---
+++ src/Board.java
@@ -0,0 +1,55 @@
+public class Board {
+    private char[][] board;
+
+    public Board() {
+        board = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    public void display() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) System.out.print("|");
+            }
+            System.out.println();
+            if (i < 2) System.out.println("-----");
+        }
+    }
+
+    public boolean isPositionEmpty(int row, int col) {
+        return board[row][col] == ' ';
+    }
+
+    public void updateBoard(int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    public boolean checkWin(char symbol) {
+        // Check rows and columns
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) return true;
+        }
+        // Check diagonals
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
+
+        return false;
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
